6ecd245a979705eda96f432c0a853e836df54f4d
Ahmed Lasisi <ahmed.lasisi@gmail.com>
2025-02-02 20:43:18 +0000

    add role-based access control to BrokerConfigController for admin users

--- src/Controller/BrokerConfigController.php
+++ src/Controller/BrokerConfigController.php
@@ -9,10 +9,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[Route('/api/brokers')]
+#[IsGranted('ROLE_ADMIN')]
 class BrokerConfigController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
