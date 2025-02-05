40d963c56b285e3cc1286a645f875e29fb05b03c
Ahmed Lasisi <ahmed.lasisi@gmail.com>
2025-02-05 12:17:33 +0000

    add DATEDIFF DQL function for date difference calculations

---
+++ src/Doctrine/DQL/DatediffFunction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Doctrine\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+class DatediffFunction extends FunctionNode
+{
+    private $firstDate;
+    private $secondDate;
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+
+        $this->firstDate = $parser->ArithmeticPrimary();
+
+        $parser->match(TokenType::T_COMMA);
+
+        $this->secondDate = $parser->ArithmeticPrimary();
+
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return sprintf(
+            'DATEDIFF(%s, %s)',
+            $this->firstDate->dispatch($sqlWalker),
+            $this->secondDate->dispatch($sqlWalker)
+        );
+    }
+}
