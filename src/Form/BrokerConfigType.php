04f9ef26f9eca95dfe5c43fac2b1bf94a5ca102f
Ahmed Lasisi <ahmed.lasisi@gmail.com>
2025-02-09 22:32:51 +0000

    refactor BrokerConfigType to utilize JsonPlaceholders for JSON placeholder and help text

--- src/Form/BrokerConfigType.php
+++ src/Form/BrokerConfigType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Form\Type\JsonType;
+use App\Util\JsonPlaceholders;
 use App\Entity\BrokerConfig;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,53 +13,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BrokerConfigType extends AbstractType
 {
-    private const JSON_PLACEHOLDER = '{
-        "PolicyNumber": "PolicyNumber",
-        "InsuredAmount": "InsuredAmount",
-        "StartDate": "StartDate",
-        "EndDate": "EndDate",
-        "AdminFee": "AdminFee",
-        "BusinessDescription": "BusinessDescription",
-        "BusinessEvent": "BusinessEvent",
-        "ClientType": "ClientType",
-        "ClientRef": "ClientRef",
-        "Commission": "Commission",
-        "EffectiveDate": "EffectiveDate",
-        "InsurerPolicyNumber": "InsurerPolicyNumber",
-        "IPTAmount": "IPTAmount",
-        "Premium": "Premium",
-        "PolicyFee": "PolicyFee",
-        "PolicyType": "PolicyType",
-        "Insurer": "Insurer",
-        "RenewalDate": "RenewalDate",
-        "RootPolicyRef": "RootPolicyRef",
-        "Product": "Product"
-    }';
-
-    private const JSON_HELP_TEXT = 'Please provide the file mapping in JSON format. Example:
-                    {
-        "PolicyNumber": "PolicyNumber",
-        "InsuredAmount": "InsuredAmount",
-        "StartDate": "StartDate",
-        "EndDate": "EndDate",
-        "AdminFee": "AdminFee",
-        "BusinessDescription": "BusinessDescription",
-        "BusinessEvent": "BusinessEvent",
-        "ClientType": "ClientType",
-        "ClientRef": "ClientRef",
-        "Commission": "Commission",
-        "EffectiveDate": "EffectiveDate",
-        "InsurerPolicyNumber": "InsurerPolicyNumber",
-        "IPTAmount": "IPTAmount",
-        "Premium": "Premium",
-        "PolicyFee": "PolicyFee",
-        "PolicyType": "PolicyType",
-        "Insurer": "Insurer",
-        "RenewalDate": "RenewalDate",
-        "RootPolicyRef": "RootPolicyRef",
-        "Product": "Product"
-    }';
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -79,9 +33,9 @@ class BrokerConfigType extends AbstractType
                 'label' => 'File Mapping (JSON Format) modify the value of each keys to match the column names in the CSV file',
                 'required' => true,
                 'attr' => [
-                    'placeholder' => self::JSON_PLACEHOLDER
+                    'placeholder' => JsonPlaceholders::BROKER_CONFIG
                 ],
-                'help' => self::JSON_HELP_TEXT
+                'help' => JsonPlaceholders::BROKER_CONFIG
             ]);
     }
 
---
+++ src/Util/JsonPlaceholders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Util;
+
+class JsonPlaceholders
+{
+    public const BROKER_CONFIG = '{
+        "PolicyNumber": "PolicyNumber",
+        "InsuredAmount": "InsuredAmount",
+        "StartDate": "StartDate",
+        "EndDate": "EndDate",
+        "AdminFee": "AdminFee",
+        "BusinessDescription": "BusinessDescription",
+        "BusinessEvent": "BusinessEvent",
+        "ClientType": "ClientType",
+        "ClientRef": "ClientRef",
+        "Commission": "Commission",
+        "EffectiveDate": "EffectiveDate",
+        "InsurerPolicyNumber": "InsurerPolicyNumber",
+        "IPTAmount": "IPTAmount",
+        "Premium": "Premium",
+        "PolicyFee": "PolicyFee",
+        "PolicyType": "PolicyType",
+        "Insurer": "Insurer",
+        "RenewalDate": "RenewalDate",
+        "RootPolicyRef": "RootPolicyRef",
+        "Product": "Product"
+    }';
+}
