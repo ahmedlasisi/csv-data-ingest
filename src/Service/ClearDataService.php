bd0e6a45793bbcf9d56d6e2a3a243374f54ee48a
Ahmed Lasisi <ahmed.lasisi@gmail.com>
2025-02-10 01:06:52 +0000

    add ClearDataService to manage broker data clearing operations

---
+++ src/Service/ClearDataService.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Broker;
+use App\Service\CacheHelper;
+use Psr\Log\LoggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ClearDataService
+{
+    private EntityManagerInterface $entityManager;
+    private LoggerInterface $logger;
+    private CacheHelper $cacheHelper;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger,
+        CacheHelper $cacheHelper
+    ) {
+        $this->entityManager = $entityManager;
+        $this->logger = $logger;
+        $this->cacheHelper = $cacheHelper;
+    }
+
+    public function clearBrokerData(Broker $broker): void
+    {
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->deleteEntities($broker, [
+                'App\Entity\Financials' => 'broker',
+                'App\Entity\Policy' => 'broker',
+                'App\Entity\BrokerClient' => 'broker',
+                'App\Entity\Insurer' => 'broker',
+                'App\Entity\Product' => 'broker',
+                'App\Entity\Event' => 'broker'
+            ]);
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            $this->logger->info("Cleared all policy-related data for broker: " . $broker->getId());
+        } catch (\Throwable $e) {
+            $this->entityManager->rollBack();
+            $this->logger->error("Failed to clear broker data: " . $e->getMessage());
+            throw new \Exception("Error clearing broker data: " . $e->getMessage());
+        }
+    }
+
+    public function clearBrokerPoliciesData(Broker $broker): void
+    {
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->deleteEntities($broker, [
+                'App\Entity\Financials' => 'broker',
+                'App\Entity\Policy' => 'broker'
+            ]);
+
+            $this->cacheHelper->invalidate("aggregation_summary");
+            $this->cacheHelper->invalidate("aggregation_by_broker_{$broker->getUuid()}");
+
+            $this->entityManager->commit();
+            $this->logger->info("Cleared policies data for broker: " . $broker->getName());
+        } catch (\Exception $e) {
+            $this->entityManager->rollback();
+            $this->logger->error("Failed to clear broker policies: " . $e->getMessage());
+            throw $e;
+        }
+    }
+
+    public function clearBrokerAllData(Broker $broker): void
+    {
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->clearBrokerPoliciesData($broker);
+
+            $this->deleteEntities($broker, [
+                'App\Entity\Client' => 'broker',
+                'App\Entity\BrokerConfig' => 'broker'
+            ]);
+
+            $this->cacheHelper->invalidate("aggregation_summary");
+            $this->cacheHelper->invalidate("aggregation_by_broker_{$broker->getUuid()}");
+
+            $this->entityManager->commit();
+            $this->logger->info("Cleared all data for broker: " . $broker->getName());
+        } catch (\Exception $e) {
+            $this->entityManager->rollback();
+            $this->logger->error("Failed to clear broker data: " . $e->getMessage());
+            throw $e;
+        }
+    }
+
+    private function deleteEntities(Broker $broker, array $entities): void
+    {
+        foreach ($entities as $entity => $field) {
+            $queryStr = "DELETE FROM $entity e WHERE e.$field = :broker";
+            $query = $this->entityManager->createQuery($queryStr);
+            $query->setParameter('broker', $broker->getId());
+            $query->execute();
+        }
+    }
+}
