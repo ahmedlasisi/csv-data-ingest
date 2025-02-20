4232f2036baed67bdd6e3455c4f1cfef435c872a
Ahmed Lasisi <ahmed.lasisi@gmail.com>
2025-02-20 13:02:37 +0000

    add broker cache refresh functionality to AggregationService

--- app/src/Service/AggregationService.php
+++ app/src/Service/AggregationService.php
@@ -7,6 +7,8 @@ use App\Repository\BrokerRepository;
 use App\Repository\PolicyRepository;
 use Symfony\Component\Cache\Attribute\Cache;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Messenger\MessageBusInterface;
+use App\Message\CacheRefreshMessage;
 
 class AggregationService
 {
@@ -14,10 +16,15 @@ class AggregationService
     private const CACHE_TTL_BROKER = 600;   // 10 minutes
 
     public function __construct(
-        private readonly BrokerRepository $brokerRepository,
-        private readonly PolicyRepository $policyRepository,
-        private readonly CacheHelper $cacheHelper
+        private  BrokerRepository $brokerRepository,
+        private  PolicyRepository $policyRepository,
+        private  CacheHelper $cacheHelper,
+        private MessageBusInterface $bus
     ) {
+        $this->brokerRepository = $brokerRepository;
+        $this->policyRepository = $policyRepository;
+        $this->cacheHelper = $cacheHelper;
+        $this->bus = $bus;
     }
 
     public function getBrokerByUuid(string $uuid): Broker
@@ -47,5 +54,29 @@ class AggregationService
     public function getAggregationDataByBroker(Broker $broker): array
     {
         return $this->policyRepository->findByBroker($broker);
+    }
+
+    /**
+    * Triggers broker cache refresh in the background.
+    */
+    public function triggerBrokerCacheRefresh(Broker $broker): void
+    {
+        $this->bus->dispatch(new CacheRefreshMessage('aggregation_summary'));
+        $this->bus->dispatch(new CacheRefreshMessage('aggregation_by_broker'));
+        $this->bus->dispatch(new CacheRefreshMessage('aggregation_broker_' . $broker->getUuid()));
+    }
+    /**
+    * Triggers cache refresh in the background.
+    */
+    public function triggerCacheRefresh(): void
+    {
+        $this->bus->dispatch(new CacheRefreshMessage('aggregation_summary'));
+        $this->bus->dispatch(new CacheRefreshMessage('aggregation_by_broker'));
+
+        $brokers = $this->brokerRepository->findAll();
+
+        foreach ($brokers as $broker) {
+            $this->bus->dispatch(new CacheRefreshMessage('aggregation_broker_' . $broker->getUuid()));
+        }
     }
 }
